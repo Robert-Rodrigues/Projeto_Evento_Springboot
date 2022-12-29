6ccedf01f48d80ca0489bf444a85e485893808cf
Robert <robert__souza@hotmail.com>
2022-12-28 21:02:18 -0300

    Conexão do formulário com o banco de dados

--- src/main/java/com/eventoapp/eventoapp/EventController.java
+++ src/main/java/com/eventoapp/eventoapp/EventController.java
@@ -1,13 +1,29 @@
 package com.eventoapp.eventoapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eventoapp.eventoapp.model.Evento;
+import com.eventosapp.repository.EventoRepository;
+
+@EnableJpaRepositories(basePackages = "com.eventosapp.repository")
 
 @Controller
 public class EventController {
 	
-	@RequestMapping("/cadastrarEvento")
+	@Autowired
+	private EventoRepository er;
+	
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.GET)
 	public String form() {
 		return"evento/formEvento";
+	}
+	@RequestMapping(value="/cadastrarEvento", method=RequestMethod.POST)
+	public String form(Evento evento) {
+		er.save(evento);
+		return"redirect:/cadastrarEvento";
 	}
 }
