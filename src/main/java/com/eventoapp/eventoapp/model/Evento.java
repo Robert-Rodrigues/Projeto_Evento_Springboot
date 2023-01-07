d14c08f768d910631da37399a2f42d0e58ffec6a
Robert <robert__souza@hotmail.com>
2023-01-07 03:49:20 -0300

    Relacionando entidades utilizando @OneToMany e @ManyToOne

--- src/main/java/com/eventoapp/eventoapp/model/Evento.java
+++ src/main/java/com/eventoapp/eventoapp/model/Evento.java
@@ -1,11 +1,13 @@
 package com.eventoapp.eventoapp.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 
 @Entity
 public class Evento implements Serializable{
@@ -26,6 +28,9 @@ public class Evento implements Serializable{
 	private String local;
 	private String data;
 	private String horario;
+	
+	@OneToMany
+	private List<Convidado> convidado;
 	
 	public String getNome() {
 		return nome;
